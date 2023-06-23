a6598bc36adba589f38d19bb12104bb14816e35f
Arya Burhan <99354272+AaronPaul6@users.noreply.github.com>
2023-06-23 22:39:32 +0300

    Add project files.

---
+++ SoftwareConstructionAssignment2/Controllers/CustomersController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SoftwareConstructionAssignment2.Models;
+
+namespace SoftwareConstructionAssignment2.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomersController : ControllerBase
+    {
+        private static List<Customer> _customers = new List<Customer>();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Customer>> GetCustomers()
+        {
+            return Ok(_customers);
+        }
+
+        [HttpPost]
+        public ActionResult<Customer> CreateCustomer(Customer customer)
+        {
+            customer.Id = _customers.Count + 1;
+            _customers.Add(customer);
+            return CreatedAtAction(nameof(GetCustomerById), new { id = customer.Id }, customer);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Customer> GetCustomerById(int id)
+        {
+            var customer = _customers.Find(c => c.Id == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return Ok(customer);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateCustomer(int id, Customer updatedCustomer)
+        {
+            var customer = _customers.Find(c => c.Id == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            customer.Name = updatedCustomer.Name;
+            customer.Email = updatedCustomer.Email;
+            customer.Phone = updatedCustomer.Phone;
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCustomer(int id)
+        {
+            var customer = _customers.Find(c => c.Id == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            _customers.Remove(customer);
+            return NoContent();
+        }
+    }
+}
